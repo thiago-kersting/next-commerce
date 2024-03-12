22de6dc7fe4c3ebd6859d258a1b7ca9b9f7483fe
Thiago Kersting Puls <thiagokpuls@gmail.com>
2024-03-12 16:31:54 -0300

    definindo tipagem para produto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
-export default function Home() {
+import { ProductType } from "@/types/ProductType";
+
+async function getProducts() {
+  const res = await fetch('https://fakestoreapi.com/products')
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch products')
+  }
+
+  return res.json()
+}
+
+
+export default async function Home() {
+  const products = await getProducts();
+  console.log(products);
   return (
-    <div>
-      <h1>Next E-Commerce 13</h1>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        {products.map((product: ProductType) => (
+          <div key={product.id}>
+            {product.title}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
