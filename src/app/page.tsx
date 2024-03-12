6d318b534226bbba96b6fa10b2e007bdb5fc15c6
Thiago Kersting Puls <thiagokpuls@gmail.com>
2024-03-12 17:13:30 -0300

    estilizando componente produto com Next Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ async function getProducts() {
 export default async function Home() {
   const products = await getProducts();
   return (
-    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+    <div className="max-w-7xl mx-auto py-8 px-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
         {products.map((product: ProductType) => (
           <Product key={product.id} product={product}></Product>
