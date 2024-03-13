c85145985e23b32ba6402ba8c1c55f4230124040
Thiago Kersting Puls <thiagokpuls@gmail.com>
2024-03-13 00:08:08 -0300

    Drawer dos produtos do carrinho

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,23 +1,28 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
+import Cart from "./Cart";
 
 function Navbar() {
+
   return (
     <nav className="fixed top-0 w-full flex items-center py-2 px-8 justify-between z-50 bg-slate-800 text-gray-300">
       <Link className="uppercase font-bold text-md h-12 flex items-center" href={"/"}>
         Next Store
       </Link>
       <div className="flex items-center gap-8">
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-        <SignedOut>
-          <SignInButton mode="modal">
-          <button className="border rounded-md border-gray-400 px-3 py-2">
-            Fazer Login
-          </button>
-          </SignInButton>
-        </SignedOut>
+        <Cart />
+        <div>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton mode="modal">
+              <button className="border rounded-md border-gray-400 px-3 py-2">
+                Fazer Login
+              </button>
+            </SignInButton>
+          </SignedOut>
+        </div>
       </div>
     </nav>
   )
