21d83365414d25b8f24f8bc235977ad5f3a92c7a
Thiago Kersting Puls <thiagokpuls@gmail.com>
2024-03-13 10:53:07 -0300

    adicionando usuario no stripe

--- src/app/api/webhooks/user/route.ts
+++ src/app/api/webhooks/user/route.ts
@@ -3,6 +3,7 @@ import { IncomingHttpHeaders } from "http";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook, WebhookRequiredHeaders } from "svix";
+import Stripe from "stripe";
 
 const webhookSecret = process.env.CLERK_WEBHOOK_SECRET || '';
 
@@ -61,10 +62,21 @@ async function handler(request: Request) {
             ...attributes
         } = evt.data;
 
+        // inserir usuario no stripe
+        const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+            apiVersion: '2023-10-16',
+        });
+
+        const customer = await stripe.customers.create({
+            name: `${first_name} ${last_name}`,
+            email: email_addresses ? email_addresses[0].email_adress : '',
+        })
+
         await prisma.user.upsert({
             where: {externalId: id as string},
             create: {
                 externalId: id as string,
+                stripeCustomerId: customer.id,
                 attributes
             },
             update: {
--- src/app/components/Hydrate.tsx
+++ src/app/components/Hydrate.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { ReactNode, useEffect, useState } from "react";
+import SkeletonCard from "./SkeletonCard";
 
 export default function Hydrate({children}: {children: ReactNode}) {
     const [isMounted, setIsMounted] = useState(false);
@@ -9,5 +10,18 @@ export default function Hydrate({children}: {children: ReactNode}) {
         setIsMounted(true);
     }, []);
 
-    return isMounted ? <>{children}</> : <span>carregando...</span>;
+    return isMounted ? <>{children}</> : (
+        <div className="max-w-7xl mx-auto py-8 px-8 xl:px-0">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+                <SkeletonCard isLoading/>
+                <SkeletonCard isLoading/>
+                <SkeletonCard isLoading/>
+                <SkeletonCard isLoading/>
+                <SkeletonCard isLoading/>
+                <SkeletonCard isLoading/>
+                <SkeletonCard isLoading/>
+                <SkeletonCard isLoading/>
+            </div>
+        </div>
+    );
 }
