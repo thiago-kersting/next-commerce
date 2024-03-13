5815d83595f9bda06c11f01f3e8eeb39b29ab7a8
Thiago Kersting Puls <thiagokpuls@gmail.com>
2024-03-12 23:23:43 -0300

    adicionando skeleton nos products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,30 @@
 import { ProductType } from "@/types/ProductType";
 import Product from "./components/Product";
+import Stripe from "stripe";
 
-async function getProducts() {
-  const res = await fetch('https://fakestoreapi.com/products')
+async function getProducts(): Promise<ProductType[]> {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: '2023-10-16',
+  });
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch products')
-  }
+  const products = await stripe.products.list();
+  const formatedProducts = await Promise.all(
+    products.data.map(async product => {
+      const price = await stripe.prices.list({
+        product: product.id,
+      });
+      return {
+        id: product.id,
+        price: price.data[0].unit_amount,
+        name: product.name,
+        image: product.images[0],
+        description: product.description,
+        currency: price.data[0].currency,
+      }
+    })
+  );
 
-  return res.json()
+  return formatedProducts;
 }
 
 
@@ -17,7 +33,7 @@ export default async function Home() {
   return (
     <div className="max-w-7xl mx-auto py-8 px-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
-        {products.map((product: ProductType) => (
+        {products.map((product) => (
           <Product key={product.id} product={product}></Product>
         ))}
       </div>
